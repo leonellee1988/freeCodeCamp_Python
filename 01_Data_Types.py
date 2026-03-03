bf0691b0ddf11c5d2f6e0566f7c76faf03ce88fd
Edwin Lee <leonellee1988@tutamail.com>
2026-03-02 22:17:58 -0600

    freeCodeCamp No. 1 02032026

---
+++ 01_Data_Types.py
@@ -0,0 +1,35 @@
+# Data types
+
+my_integer_var = 10
+print(type(my_integer_var))
+
+my_float_var = 4.50
+print(type(my_float_var))
+
+my_string_var = 'Hello World!'
+print(type(my_string_var))
+
+my_boolean_var = True
+print(type(my_boolean_var))
+
+my_set_var = {5, 7, 11}
+print(type(my_set_var))
+
+my_dictionary_var = {'name': 'Alice', 'age': 25}
+print(type(my_dictionary_var))
+
+my_tuple_var = (13, 17, 19)
+print(type(my_tuple_var))
+
+my_range_var = range(10)
+print(type(my_range_var))
+
+my_list_var = [22, 'Hello World!', False, (23, 29)]
+print(type(my_list_var))
+
+my_none_var = None
+print(type(my_none_var))
+
+# The built-in instance function
+
+print(isinstance('Hello World!', str))
